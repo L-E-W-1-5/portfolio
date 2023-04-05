aa4c15921cbe3bdb3f2646d6def397ea34d2735d
L-E-W-1-5 <lewiswootton88@hotmail.com>
2023-04-05 17:41:10 +0100

    icons need updating, start menu click outside done

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react';
+
+const Clock = () => {
+
+    const [time, setTime] = useState(new Date());
+
+    const refreshClock = () => {
+        setTime(new Date());
+    }
+
+    useEffect(() => {
+        const timerId = setInterval(refreshClock, 1000);
+        return function cleanup() {
+            clearInterval(timerId);
+        };
+    }, []);
+
+    return (
+        <span>{time.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</span>
+    )
+
+
+};
+
+export default Clock;
