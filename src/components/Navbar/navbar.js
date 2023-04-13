035ce114a47db3198c05ec996b7862aa545eda96
L-E-W-1-5 <lewiswootton88@hotmail.com>
2023-04-13 01:11:05 +0100

    start menu next.. NEED THE CONTENTgit add .

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,9 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import Clock from '../Clock/Clock.js';
-import pdf from '../../data/Mr_Lewis Wootton_Resume_28-03-2023-21-09-28.pdf';
+//import pdf from '../../data/Mr_Lewis Wootton_Resume_28-03-2023-21-09-28.pdf';
 import windowsIcon from '../../assets/windows_icon.png';
+import {StartMenu} from '../StartMenu/StartMenu.js';
 // import executable from '../../assets/executable.png';
 // import recycle from '../../assets/recycle.png';
 // import wordpad from '../../assets/wordpad.png';
@@ -13,8 +14,7 @@ import windowsIcon from '../../assets/windows_icon.png';
 export function Navbar({tabs, setNewTarget, newTarget, minimise}) {
 
     const [startMenu, setStartMenu] = useState(false);
-    const wrapperRef = useRef(null);
-    useOutsideAlert(wrapperRef);
+   
 
     const handleMinimiseFocus = (tab) => {
         if (newTarget === tab.key || tab.minimised === true){
@@ -28,55 +28,45 @@ export function Navbar({tabs, setNewTarget, newTarget, minimise}) {
         }
     }
 
-    function useOutsideAlert(ref) {
-        useEffect(() => {
-            function handleClickOutside(event) {
-                if (ref.current && !ref.current.contains(event.target)) {
-                    setStartMenu(false);
-                }
-            }
-
-            document.addEventListener('mousedown', handleClickOutside);
-            return () => {
-                document.removeEventListener('mousedown', handleClickOutside);
-            }
-        }, [ref])
-    }
-
-   
-
 
     return (
-        <div className="nav-menu border-shading">
+      <div className="nav-menu border-shading">
+        {startMenu && <StartMenu setStart={setStartMenu}></StartMenu>}
 
-            {startMenu && <div ref={wrapperRef} className="start-menu">
-                <a href="#about_me_link">About Me</a>
-                <a href="#projects_link">Projects</a>
-                <a href={pdf}>CV</a>
-            </div>}
+        <div className="nav-container">
+          <div
+            className="start-button border-shading"
+            id={startMenu === true ? "tab-highlight" : ""}
+            onClick={() => setStartMenu((current) => !current)}
+          >
+            <img
+              className="start-button-icon"
+              src={windowsIcon}
+              alt="windows icon"
+            ></img>
+            Start
+          </div>
 
-            <div className="nav-container">
+          <div className="nav-tab">
+            {tabs.map((tab, k) => {
+              return (
+                <button
+                  className="tab-div border-shading"
+                  id={newTarget === tab.key ? "tab-highlight" : ""}
+                  key={k}
+                  onClick={() => handleMinimiseFocus(tab)}
+                >
+                  <img className="tab-icon" src={tab.icon} alt="T"></img>
+                  <p className="tab-text">{tab.data}</p>
+                </button>
+              );
+            })}
+          </div>
 
-                <div className="start-button border-shading" id={startMenu === true ? "tab-highlight" : ""} onClick={() => setStartMenu(current => !current)}>
-                <img className="start-button-icon" src={windowsIcon} alt="windows icon"></img>
-                Start
-                </div>       
-
-                <div className="nav-tab">
-                    {tabs.map((tab, k) => {
-                        return <button className="tab-div border-shading" id={newTarget === tab.key ? "tab-highlight" : ""} key={k} onClick={() => handleMinimiseFocus(tab)}><img className="tab-icon" src={tab.icon} alt="T"></img><p className="tab-text">{tab.data}</p></button>
-                    })}
-                </div>
-
-                <div className="nav-clock">
-                    <Clock></Clock>
-                </div>
-
-            </div>
-
-            
-        
-            
+          <div className="nav-clock">
+            <Clock></Clock>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
---
+++ src/components/StartMenu/StartMenu.js
@@ -0,0 +1,36 @@
+import pdf from '../../data/Mr_Lewis Wootton_Resume_28-03-2023-21-09-28.pdf';
+import {useEffect, useRef} from 'react';
+import './StartMenu.css';
+
+export const StartMenu = ({setStart}) => {
+
+    const wrapperRef = useRef(null);
+    useOutsideAlert(wrapperRef);
+
+    function useOutsideAlert(ref) {
+      useEffect(() => {
+        function handleClickOutside(event) {
+          if (ref.current && !ref.current.contains(event.target)) {
+            setStart(false);
+          }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, [ref]);
+    }
+
+    return (
+      <div ref={wrapperRef} className="start-menu">
+        <div className="start-menu-title">
+            <h2>Windows 95</h2>
+        </div>
+        <div className="start-menu-list">
+          <a href="#about_me_link">About Me</a>
+          <a href="#projects_link">Projects</a>
+          <a href={pdf}>CV</a>
+        </div>
+      </div>
+    );
+}
